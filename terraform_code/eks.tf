a86099003c58747b3e865f0b39fe4e0accb2294d
juanploaiza <Juan_Pablo_Loaiza@mckinsey.com>
2025-01-08 13:33:07 -0600

    Adding EKS cluster.

--- terraform_code/eks.tf
+++ terraform_code/eks.tf
@@ -1,12 +1,13 @@
 module "sgs" {
-  source         = "modules/sg_eks"
+  source         = "./modules/sg_eks"
   vpc_id         = aws_vpc.main.id
   workstation_ip = var.workstation_ip
 }
 
 module "eks" {
-  source     = "modules/eks"
+  source     = "./modules/eks"
   vpc_id     = aws_vpc.main.id
   subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-  sg_ids     = module.sgs.security_group_public
+  sg_ids     = module.sgs.
+  key_name  = var.key_name
 }
